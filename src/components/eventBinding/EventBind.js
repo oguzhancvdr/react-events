774d7bfd41332ac6e0d162a30c013a9954903676
ogzhn <oguzhancavdar06@gmail.com>
2021-04-26 11:15:55 +0300

    created EventBind comp which is class based

---
+++ src/components/eventBinding/EventBind.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+export default class EventBind extends Component {
+   
+    render() {
+        return (
+            <div>
+               <h2>EventBinding</h2>
+            </div>
+        )
+    }
+}
+
