5ee881e27a5c3de23d461b53969d12f0df4e6eb4
ogzhn <oguzhancavdar06@gmail.com>
2021-04-27 00:56:34 +0300

    added onSubmit event

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './app.css'
 
 // import EventBind from './components/eventBinding/EventBind'
 import MouseEvents from './components/mouseEvents/MouseEvents'
-
+import KeyboardEvents from './components/keyboardEvents/KeyboardEvents'
+import Form from './components/form/Form'
 const App = () => {
     return (
         <div className="App">
             {/* <h1>Events in React</h1> */}
             {/* <EventBind /> */}
-            <MouseEvents />
+            {/* <MouseEvents /> */}
+            {/* <KeyboardEvents /> */}
+            <Form />
         </div>
     )
 }
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,53 +1,85 @@
-
+import { useState } from "react";
+import {setInput} from '../../helper/inputHelper'
 import "./styles.css";
 
 const Form = () => {
-  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [country, setCountry] = useState("");
+
+  // const handleName = (e) => setName(e.target.value);
+  // const handleEmail = (e) => setEmail(e.target.value);
+  // const handlePassword = (e) => setPassword(e.target.value);
+  // const handleCountry = (e) => setCountry(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`
+    userName: ${name}
+    email: ${email}
+    password: ${password}
+    country: ${country};    
+    `);
+    setName("");
+    setEmail("");
+    setPassword("");
+    setCountry("");
+  };
+  // const setInput = (setter) => (e) => setter(e.target.value);
+
   return (
     <div style={{ height: "110vh" }}>
-      <form >
+      <form onSubmit={handleSubmit}>
         <div>
-          <p></p>
+          <p>{name}</p>
           <label>
             <strong>Username:</strong>{" "}
           </label>
           <br />
           <input
+            value={name}
             type="text"
             placeholder="name"
+            onChange={setInput(setName)}
           />
         </div>
         <div>
-          <p></p>
+          <p>{email}</p>
           <label>
             <strong>E-mail:</strong>{" "}
           </label>
           <br />
           <input
-            type="text"
-            placeholder="mail"
+            value={email}
+            type="email"
+            placeholder="email"
+            onChange={setInput(setEmail)}
           />
         </div>
         <div>
-          <p></p>
+          <p>{password}</p>
           <label>
             <strong>Password: </strong>
           </label>
           <br />
           <input
+            value={password}
             type="password"
             placeholder="pass"
+            onChange={setInput(setPassword)}
           />
         </div>
         <div style={{ margin: "10px auto" }}>
-          <p></p>
+          <p>{country}</p>
           <label>
             <strong>Country: </strong>
           </label>
           <select
+            value={country}
+            onChange={setInput(setCountry)}
             style={{ marginTop: "10px", width: "100px", padding: "10px" }}
           >
-            <option value="" >Country</option>
+            <option value="">Country</option>
             <option value="Turkey">Turkey</option>
             <option value="Germany">Germany</option>
             <option value="Netherland">Netherland</option>
