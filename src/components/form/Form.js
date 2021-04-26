a4a39b4d3eb88286e86f0b3aaae14728a8e9650b
ogzhn <oguzhancavdar06@gmail.com>
2021-04-26 11:16:10 +0300

    created Form component

---
+++ src/components/form/Form.js
@@ -0,0 +1,75 @@
+
+import "./styles.css";
+
+const ClickEvents = () => {
+  
+  return (
+    <div style={{ height: "110vh" }}>
+      <form >
+        <div>
+          <p></p>
+          <label>
+            <strong>Username:</strong>{" "}
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="name"
+
+          />
+        </div>
+        <div>
+          <p></p>
+          <label>
+            <strong>E-mail:</strong>{" "}
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="mail"
+          />
+        </div>
+        <div>
+          <p></p>
+          <label>
+            <strong>Password: </strong>
+          </label>
+          <br />
+          <input
+            type="password"
+            placeholder="pass"
+          />
+        </div>
+        <div style={{ margin: "10px auto" }}>
+          <p></p>
+          <label>
+            <strong>Country: </strong>
+          </label>
+          <select
+            style={{ marginTop: "10px", width: "100px", padding: "10px" }}
+          >
+            <option value="" >Country</option>
+            <option value="Turkey">Turkey</option>
+            <option value="Germany">Germany</option>
+            <option value="Netherland">Netherland</option>
+            <option value="France">France</option>
+          </select>
+        </div>
+        <button className="btn__click" type="submit">
+          submit
+        </button>
+      </form>
+      {/* <div>
+        <a
+          style={{ position: "fixed", bottom: "0", marginBottom: "20px" }}
+          onClick={handleClick}
+          href="www.example.com"
+        >
+          Example
+        </a>
+      </div> */}
+    </div>
+  );
+};
+
+export default ClickEvents;
