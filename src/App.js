52631be671d7d2fe92b52794f863cbc859c97694
ogzhn <oguzhancavdar06@gmail.com>
2021-08-09 20:46:08 +0300

    event binding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import EventBind from './components/eventBinding/EventBind'
+import EventBind from './components/eventBinding/EventBind'
 // import MouseEvents from './components/mouseEvents/MouseEvents'
 // import KeyboardEvents from './components/keyboardEvents/KeyboardEvents'
 // import Form from './components/form/Form'
@@ -7,8 +7,8 @@ import './App.css'
 const App = () => {
     return (
         <div className="App">
-            <h1>Events in React</h1>
-            {/* <EventBind /> */}
+            {/* <h1>Events in React</h1> */}
+            <EventBind />
             {/* <MouseEvents /> */}
             {/* <KeyboardEvents /> */}
             {/* <Form /> */}
--- src/components/eventBinding/EventBind.js
+++ src/components/eventBinding/EventBind.js
@@ -1,3 +1,15 @@
+
+
+import { Component } from "react";
+
+class EventBind extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      message: "Hello"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
   /**
    * TODO List
    * create class component
@@ -7,4 +19,21 @@
    * 4. Class property as an arrow function
    */
 
+  handleClick(){
+    this.setState({message: "Goodbye!"})
+    console.log("this: ", this);
+  }
 
+  render(){
+    return(
+      <div>
+        <p>{this.state.message}</p>
+        {/* <button onClick={this.handleClick.bind(this)}>click</button> */}
+        {/* <button onClick={() => this.handleClick()}>click</button> */}
+        <button onClick={this.handleClick}>click</button>
+      </div>
+    )
+  }
+}
+
+export default EventBind;
